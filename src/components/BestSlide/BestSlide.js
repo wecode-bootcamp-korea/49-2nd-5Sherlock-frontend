c0551bc207fb6518e6e10da593c0bdfc289a3ddd
sseyeon2 <sseyoun2@gmail.com>
2023-10-04 12:39:26 +0900

    메인페이지 베스트 카테고리 수정

--- src/components/BestSlide/BestSlide.js
+++ src/components/BestSlide/BestSlide.js
@@ -1,70 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './BestSlide.scss';
+import ProductImgBox from '../../components/ProductImgBox/ProductImgBox';
 
 const BestSlide = props => {
-  const slidesInfo = [
-    {
-      bestImg: '/images/main/best-slide1.png',
-      bestName: '프리미엄 녹차와플',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide2.png',
-      bestName: '그린티 랑드샤',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide3.png',
-      bestName: '그린티 웨하스',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide4.png',
-      bestName: '메모리 인 제주 20입',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide5.png',
-      bestName: '프리미엄 티 컬렉션',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide6.png',
-      bestName: '난꽃향 세트',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide7.png',
-      bestName: '시크릿 티 스토리',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-    {
-      bestImg: '/images/main/best-slide8.png',
-      bestName: '시그니처 비스킷',
-      bestPrice: '9,000',
-      bestDiscount: '25%',
-      bestDiscountPrice: '8,100',
-    },
-  ];
-
+  const { data } = props;
   const slideWidth = 244;
 
+  const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [translateX, setTranslateX] = useState(0);
+
+  const goToDetail = id => {
+    navigate(`/product-detail/${id}`);
+  };
+
   useEffect(() => {
     const intervalId = setInterval(goToNextSlide, 3000);
     return () => clearInterval(intervalId);
@@ -72,13 +22,13 @@ const BestSlide = props => {
 
   const goToPreviousSlide = () => {
     setCurrentIndex(prevIndex =>
-      prevIndex === 0 ? slidesInfo.length - 1 : prevIndex - 1,
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1,
     );
   };
 
   const goToNextSlide = () => {
     setCurrentIndex(prevIndex =>
-      prevIndex === slidesInfo.length - 1 ? 0 : prevIndex + 1,
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1,
     );
   };
 
@@ -86,7 +36,7 @@ const BestSlide = props => {
     setTranslateX(-currentIndex * slideWidth);
   }, [currentIndex]);
 
-  const duplicatedSlides = [...slidesInfo, ...slidesInfo];
+  const duplicatedSlides = [...data, ...data];
 
   return (
     <div className="BestSlide">
@@ -102,27 +52,20 @@ const BestSlide = props => {
           }}
         >
           {duplicatedSlides.map((slide, index) => (
-            <li
-              key={index}
-              className={
-                index === currentIndex ||
-                (index === currentIndex + slidesInfo.length &&
-                  currentIndex === 0)
-                  ? 'active'
-                  : ''
-              }
-              style={{ width: `${slideWidth}px` }}
-            >
-              <p className="slideImg">
-                <img src={slide.bestImg} alt="이미지" />
-              </p>
-              <div className="productInfo">
-                <p className="bestName">{slide.bestName}</p>
-                <span className="originalPrice">{slide.bestPrice}원</span>
+            <li>
+              <ProductImgBox data={data[0]} />
+              <div
+                className="productInfo"
+                onClick={() => {
+                  goToDetail(slide.id);
+                }}
+              >
+                <p className="bestName">{slide.name}</p>
+                <span className="originalPrice">{slide.originalPrice}원</span>
                 <p className="price">
-                  {slide.bestDiscountPrice}원
+                  {slide.price}원
                   <span className="discount">
-                    &nbsp;&nbsp;{slide.bestDiscount}
+                    &nbsp;&nbsp;{slide.discountRate}%
                   </span>
                 </p>
               </div>
